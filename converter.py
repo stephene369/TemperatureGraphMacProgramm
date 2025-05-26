3e85083fece62a0be428ea9233e6ff2735448602
Stephene WANTCHEKON <stephenew36@gmail.com>
2025-05-26 19:19:10 +0100

    Version-26/05/2025/19-19-10

---
+++ converter.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+img = Image.open(r"ui\assets\img\logo.png")
+img.save(r"ui\assets\img\logo.bmp")
--- ui/js/splash.js
+++ ui/js/splash.js
@@ -1,41 +1,51 @@
-// Animations Lottie pour le splash screen
-try {
-  document.addEventListener("DOMContentLoaded", function() {
-    try {
-      const lottieAnimations = [
+// // Animations Lottie pour le splash screen
+// try {
+//   document.addEventListener("DOMContentLoaded", function() {
+//     try {
+//       const lottieAnimations = [
 
-        "lotties/onMASlImNg.lottie"
-        // "https://lottie.host/feae722c-0a85-4fbb-acdb-debe06e26ef9/8vr4dMpNX1.lottie",
-        // "https://lottie.host/18a34c71-0d92-4e05-ae83-e9281702aa6d/ne6UxpW4xy.lottie",
-      //   "https://lottie.host/e6972592-361b-4e15-8ede-5e9bf3c062e3/MOKWNpsqcr.lottie"
-      ];
+//         "lotties/onMASlImNg.lottie"
+//         // "https://lottie.host/feae722c-0a85-4fbb-acdb-debe06e26ef9/8vr4dMpNX1.lottie",
+//         // "https://lottie.host/18a34c71-0d92-4e05-ae83-e9281702aa6d/ne6UxpW4xy.lottie",
+//       //   "https://lottie.host/e6972592-361b-4e15-8ede-5e9bf3c062e3/MOKWNpsqcr.lottie"
+//       ];
       
-      const randomAnimation = lottieAnimations[Math.floor(Math.random() * lottieAnimations.length)];
+//       const randomAnimation = lottieAnimations[Math.floor(Math.random() * lottieAnimations.length)];
       
-      const lottieContainer = document.getElementById('lottie-container');
-      const lottiePlayer = document.createElement('dotlottie-player');
+//       const lottieContainer = document.getElementById('lottie-container');
+//       const lottiePlayer = document.createElement('dotlottie-player');
       
-      lottiePlayer.setAttribute('src', randomAnimation);
-      lottiePlayer.setAttribute('background', 'transparent');
-      lottiePlayer.setAttribute('speed', '1');
-      lottiePlayer.setAttribute('style', 'width: 250px; height: 250px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);');
-      lottiePlayer.setAttribute('loop', '');
-      lottiePlayer.setAttribute('autoplay', '');
+//       lottiePlayer.setAttribute('src', randomAnimation);
+//       lottiePlayer.setAttribute('background', 'transparent');
+//       lottiePlayer.setAttribute('speed', '1');
+//       lottiePlayer.setAttribute('style', 'width: 250px; height: 250px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);');
+//       lottiePlayer.setAttribute('loop', '');
+//       lottiePlayer.setAttribute('autoplay', '');
       
-      lottieContainer.appendChild(lottiePlayer);      
-      setTimeout(function() {
-        try {
-          const splashScreen = document.getElementById('splash-screen');
-          splashScreen.classList.add('hidden');
+//       lottieContainer.appendChild(lottiePlayer);      
+//       setTimeout(function() {
+//         try {
+//           const splashScreen = document.getElementById('splash-screen');
+//           splashScreen.classList.add('hidden');
             
-        } catch (error) {
-          console.error("Erreur lors de la gestion du splash screen:", error);
-        }
-      }, 2500);
-    } catch (error) {
-      console.error("Erreur lors de l'initialisation de l'animation:", error);
-    }
-  });
-} catch (error) {
-  console.error("Erreur lors de l'ajout de l'écouteur DOMContentLoaded:", error);
-}
+//         } catch (error) {
+//           console.error("Erreur lors de la gestion du splash screen:", error);
+//         }
+//       }, 2500);
+//     } catch (error) {
+//       console.error("Erreur lors de l'initialisation de l'animation:", error);
+//     }
+//   });
+// } catch (error) {
+//   console.error("Erreur lors de l'ajout de l'écouteur DOMContentLoaded:", error);
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const splash = document.getElementById('splash-screen');
+    const mainContent = document.getElementById('main-content');
+
+    splash.style.display = 'none';
+    mainContent.classList.remove('hidden');
+  }, 2500);
+});
